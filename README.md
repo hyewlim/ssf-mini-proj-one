31bc6a268d4d9a4bfd2385ef4192887b9b579609
hyewlim <hyew.lim@gmail.com>
2022-08-15 11:36:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ssf-mini-proj-one" 
---
+++ src/main/java/com/project/VttpMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpMiniProjectApplication.class, args);
+	}
+
+}
